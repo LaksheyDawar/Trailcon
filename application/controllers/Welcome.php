e42bfa62b3000b08b38c72c7013ca8efb0990169
llakshey <lakshey.dawar@yahoo.com>
2019-04-07 00:44:25 -0400

    Part1

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+
 	public function index()
 	{
-		$this->load->view('index');
+		$arr = array();
+		
+		for ( $i=1; $i<=100; $i++){	
+		
+			if( $i%3 == 0  && $i%5 == 0){
+				$arr[$i] = "TrailconLeasing";
+			}
+			elseif($i%3 == 0){
+				$arr[$i] ="Trailcon";
+			}
+			elseif($i%5 == 0){
+				$arr[$i]= "Leasing";
+			}
+			else{
+				$arr[$i]= $i; 
+			}
+	}
+	$data['arr']=$arr;
+
+	$this->load->view('index', $data);
 	}
 }
--- application/views/index.php
+++ application/views/index.php
@@ -1,17 +1,16 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-</head>
+<!DOCTYPE html>
+<html>
+<head><title> Trailcon Leasing </title></head>
 <body>
+<h1></h1>
+<?php
+$i = 1;
+foreach ($arr as $value){ ?>
+<?php echo $i.": ". $value; ?> <br/>
+<?php 
+$i++;
+} ?>
 
-<div id="container">
-<h1>Hello World!!</h1>
-</div>
 
 </body>
 </html>
